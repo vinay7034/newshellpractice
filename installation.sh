4d49785692e472acc4445d070b0d3af7cf7d22ec
vinay <v@gmail.com>
2026-03-04 16:16:51 +0530

    codecommited

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "ERROR:: Please run this script with root privelage"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mysql is failure"
+else 
+    echo "mysql is successfully installed"
+fi
