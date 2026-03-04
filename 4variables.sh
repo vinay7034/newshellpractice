c33bed8c71708035470ebd6a21fdd15546817a2c
vinay <v@gmail.com>
2026-03-04 10:22:21 +0530

    codecommited

---
+++ 4variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1   
+PERSON2=$2    
+
+echo "$PERSON1::hello $PERSON2"
+echo "$PERSON2::how r u $PERSON1"
+echo "$PERSON1::m good $PERSON2"
+echo "$PERSON2::good to see you $PERSON1"
