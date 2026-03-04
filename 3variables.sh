47a953d8b1f3cbda708f641dddc4f3e448a9ff91
vinay <v@gmail.com>
2026-03-04 10:15:12 +0530

    codecommited

---
+++ 3variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=john
+PERSON2=raheem      
+
+echo "$PERSON1::hello $PERSON2"
+echo "$PERSON2::how r u $PERSON1"
+echo "$PERSON1::m good $PERSON2"
+echo "$PERSON2::good to see you $PERSON1"
