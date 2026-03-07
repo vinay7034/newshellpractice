92094a0b7e5a05a6baf60fa6f5113eaa76b3d93c
vinay <v@gmail.com>
2026-03-07 21:31:09 +0530

    new

--- installation.sh
+++ installation.sh
@@ -3,13 +3,15 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelage"
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing mysql is failure"
-else 
-    echo "mysql is successfully installed"
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
 fi
