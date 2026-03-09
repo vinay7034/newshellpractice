9413805e2acfbfca89ca4731bb1234578e5120ec
vinay <v@gmail.com>
2026-03-09 12:09:02 +0530

    super

--- demo1.sh
+++ demo1.sh
@@ -6,6 +6,13 @@ if [ $NUMBER -lt 1000 ] ; then
 
     echo "given $NUMBER is less than 1000"
 
+elif [ $NUMBER -eq 1000]; then
+
+    echo "given $NUMBER is equal to 1000"
+
+
+
+
 else
 
     echo "given $NUMBER is  greater than 1000"
